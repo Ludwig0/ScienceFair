e5852a86b74506beaf4c906853eb65b235a99137
Jeff Putlock <robthatroast@gmail.com>
2017-05-21 17:45:22 -0400

    made the question parser

--- QuestionSet.py
+++ QuestionSet.py
@@ -1,51 +1,38 @@
-#This one's for you, Brett <3
-#Format of test files: QUESTION|ANSWER
+# This is Jeff's better version
 
-#imports for random functions
+import sys
 import random
-#import to find the path to the questions.txt file
-import os
 
-class question_set(object):
+
+class Question:
+    def __init__(self, question, answer_index, possible_answers):
+        self.question = question
+        self.answer_index = answer_index
+        self.possible_answers = possible_answers
+
+
+class QuestionSet:
     def __init__(self):
-        # init function
-        self.questions = []
-        self.answers = []
-        self.used_questions = set()
+        self.question_list = []
+        self.current_index = 0
 
-    def construct(self):
-        #Try to find the questions.txt file and set as path
+    def parse_file(self, file_name):
         try:
-            question_file = os.path.abspath("questions.txt")
-        #If no question file, throws an exception
-        except:
-            print("No question file exists!")
+            file = open(file_name)
+            for line in file:
+                parts = line.split("|")
+                answers = parts[2].split("\\")
+                answers[len(answers)-1] = answers[len(answers)-1].rstrip()
+                self.question_list.append(Question(parts[0], int(parts[1]), answers))
+            file.close()
+        except FileNotFoundError as file_error:
+            print(sys.exc_info()[0])
 
-        #Open the file
-        file = open("questions.txt", "r")
-        for line in file:
-            #Splits file into question and answer
-            line = line.split("|")
-            #Adds questions into one list, answers into another
-            self.questions.append(str(line[0]))
-            self.answers.append(line[1])
+    def get_next_question(self):
+        self.current_index = random.randint(0, len(self.question_list)) - 1
+        if not self.has_next_question():
+            return
+        return self.question_list.pop(self.current_index)
 
-    def getQuestion(self, number):
-        #Fetches question and corresponding answers from both lists
-        question = self.questions[number]
-        answer = self.answers[number]
-        #Adds the fetched question into the used_questions list
-        #WARNING: MAY BE BUGGY, NEEDS TO BE CHECKED OUT
-        self.used_questions.add(number)
-        return question, answer
-
-    def getNewQuestion(self):
-        #If we have used all questions, returns -1
-        if list(self.used_questions) == list(range(0, len(questions))):
-            return -1
-        else:
-            #Gets list of ununsed questions using a list comprehension
-            unused_questions = [number for number in range(0, len(self.questions)) if number not in self.used_questions]
-            #Randomly picks a question number and returns it
-            question_number = random.choice(unused_questions)
-            return question_number
+    def has_next_question(self):
+        return len(self.question_list) > 0
